339a030a6c5d5c9dc68c6abf8ac53d3d0da62945
Ajayi Obaloluwa Emmanuel <obaloluwaajayi2006@gmail.com>
2026-08-17 07:01:25 +0100

    Add new surgery route handlers for pre-op and consent

--- src/modules/surgery/surgery.routes.ts
+++ src/modules/surgery/surgery.routes.ts
@@ -9,8 +9,14 @@ router.use(authenticate);
 router.post('/', (req, res, next) => surgeryController.scheduleCase(req, res, next));
 router.get('/', (req, res, next) => surgeryController.getCases(req, res, next));
 router.get('/:id', (req, res, next) => surgeryController.getCaseById(req, res, next));
-router.patch('/:id/checklist', (req, res, next) => surgeryController.updateChecklist(req, res, next));
+
+router.patch('/:id/pre-op', (req, res, next) => surgeryController.updatePreOp(req, res, next));
+router.patch('/:id/consent', (req, res, next) => surgeryController.updateConsent(req, res, next));
+router.patch('/:id/who-checklist', (req, res, next) => surgeryController.updateWHOChecklist(req, res, next));
+router.post('/:id/vitals', (req, res, next) => surgeryController.addVitalsLog(req, res, next));
+
 router.patch('/:id/start', (req, res, next) => surgeryController.startSurgery(req, res, next));
+router.patch('/:id/intraop-docs', (req, res, next) => surgeryController.updateIntraopDocs(req, res, next));
 router.patch('/:id/complete', (req, res, next) => surgeryController.completeSurgery(req, res, next));
 router.patch('/:id/cancel', (req, res, next) => surgeryController.cancelCase(req, res, next));
 
